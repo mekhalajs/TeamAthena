3f0e8b871300c74ceecc4105457b72a60a82ab7c
mekhalajs <mekhalajs@gmail.com>
2018-10-20 11:20:25 -0400

    Adding the project structure

---
+++ healthyMind/src/main/java/com/healthyMind/healthyMind/HealthyMindApplication.java
@@ -0,0 +1,12 @@
+package com.healthyMind.healthyMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyMindApplication.class, args);
+	}
+}
---
+++ healthyMind/src/test/java/com/healthyMind/healthyMind/HealthyMindApplicationTests.java
@@ -0,0 +1,16 @@
+package com.healthyMind.healthyMind;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HealthyMindApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
